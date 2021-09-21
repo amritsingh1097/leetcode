78cdbc99cfccb9f0df7ab9221df693c7efaf28dc
Amrit Singh <amrit.1097@gmail.com>
2021-09-21 22:31:02 +0530

    Updated: Unique Number of Occurrences

--- 1207. Unique Number of Occurrences/Solution.java
+++ 1207. Unique Number of Occurrences/Solution.java
@@ -1,5 +1,5 @@
-// Runtime: 1 ms, faster than 99.71% of Java online submissions for Unique Number of Occurrences.
-// Memory Usage: 36.9 MB, less than 76.65% of Java online submissions for Unique Number of Occurrences.
+// Runtime: 1 ms, faster than 99.70% of Java online submissions for Unique Number of Occurrences.
+// Memory Usage: 36.7 MB, less than 92.47% of Java online submissions for Unique Number of Occurrences.
 
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
@@ -8,11 +8,6 @@ class Solution {
             mp.put(n, mp.getOrDefault(n, 0) + 1);
         }
         
-        Set<Integer> s = new HashSet<>();
-        for (Map.Entry<Integer, Integer> e : mp.entrySet()) {
-            s.add(e.getValue());
-        }
-        
-        return s.size() == mp.size();
+        return (new HashSet<>(mp.values()).size()) == mp.size();
     }
 }
