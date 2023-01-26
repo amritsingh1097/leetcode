4729485a9f0f81097bd95411e962419f49890dc3
Amrit Singh <amrit.1097@gmail.com>
2023-01-26 18:13:37 +0530

    Added: Number of Distinct Averages

---
+++ 2465. Number of Distinct Averages/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int distinctAverages(int[] nums) {
+        Arrays.sort(nums);
+
+        Set<Double> s = new HashSet<>();
+        for (int i = 0; i < nums.length; ++i) {
+            double avg = (nums[i] + nums[nums.length - i - 1]) / 2.0;
+            s.add(avg);
+        }
+
+        return s.size();
+    }
+}
