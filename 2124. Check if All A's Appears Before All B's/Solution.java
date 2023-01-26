250631f029f17550cf820036ccb068651a744359
Amrit Singh <amrit.1097@gmail.com>
2023-01-26 18:29:46 +0530

    Added: Check if All A's Appears Before All B's

---
+++ 2124. Check if All A's Appears Before All B's/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean checkString(String s) {
+        for (int i = 1; i < s.length(); ++i) {
+            if (s.charAt(i - 1) > s.charAt(i)) return false;
+        }
+
+        return true;
+    }
+}
